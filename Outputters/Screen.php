c2904df0a2cf72ff046d907fe9fd8597781dd8e2
Toby Powell-Blyth <toby@thegiltclub.com>
2017-10-16 10:50:34 +0100

    Last minute fix

--- Outputters/Screen.php
+++ Outputters/Screen.php
@@ -7,7 +7,7 @@ class Screen extends Base
      * Simply drops the content on the screen
      * @return boolean
      */
-    function outputData()
+    function outputData():bool
     {
         echo "\n\n".sprintf($this->message, $this->dataPoint)."\n\n";
         return true;
